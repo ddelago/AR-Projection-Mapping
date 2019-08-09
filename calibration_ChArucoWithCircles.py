1b6e6c734802710c6003357b81b12a7cced15348
Daniel Delago <ddelago0596@gmail.com>
2019-08-09 08:27:12 -0700

    restructure

---
+++ calibration_ChArucoWithCircles.py
@@ -0,0 +1,334 @@
+# See Calibration method here:
+# https://www.morethantechnical.com/2017/11/17/projector-camera-calibration-the-easy-way/
+
+import numpy as np
+import cv2
+from cv2 import aruco
+import pickle
+import glob
+import os
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+gray = None
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+imgpointsProj = [] # Reprojected points.
+
+# ChAruco board variables
+CHARUCOBOARD_ROWCOUNT = 8
+CHARUCOBOARD_COLCOUNT = 6 
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_50)
+
+# Create constants to be passed into OpenCV and Aruco methods
+CHARUCO_BOARD = aruco.CharucoBoard_create(
+		squaresX=CHARUCOBOARD_COLCOUNT,
+		squaresY=CHARUCOBOARD_ROWCOUNT,
+		squareLength=0.04,
+		markerLength=0.02,
+		dictionary=ARUCO_DICT)
+
+# Constant parameters used in Aruco methods
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+
+# Create the arrays and variables we'll use to store info like corners and IDs from images processed
+corners_all = [] # Corners discovered in all images processed
+ids_all = [] # Aruco ids corresponding to corners discovered
+image_size = None # Determined at runtime
+
+cameraMatrix, distCoeffs, rvecs, tvecs = None, None, None, None
+
+# Check for camera calibration data
+if not os.path.exists('./calibration/MarkerCalibration.pckl'):
+    # images = glob.glob('./pictures/*.jpg')
+	cap = cv2.VideoCapture('ChAruco_Circles.webm')
+	validCaptures = 0
+
+	# Loop through images glob'ed
+	while cap.isOpened():
+		# Open the image
+		# img = cv2.imread(iname)
+
+		ret, img = cap.read()
+		if ret is False:
+			break
+
+		# Grayscale the image
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+		# Find aruco markers in the query image
+		corners, ids, _ = aruco.detectMarkers(
+				image=gray,
+				dictionary=ARUCO_DICT)
+		
+		if ids is None:
+			continue
+
+		# Outline the aruco markers found in our query image
+		img = aruco.drawDetectedMarkers(
+				image=img, 
+				corners=corners)
+
+		# Get charuco corners and ids from detected aruco markers
+		response, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(
+				markerCorners=corners,
+				markerIds=ids,
+				image=gray,
+				board=CHARUCO_BOARD)
+
+		# If a Charuco board was found, let's collect image/corner points
+		# Requiring at least 20 squares
+		if response > 20:
+			# Add these corners and ids to our calibration arrays
+			corners_all.append(charuco_corners)
+			ids_all.append(charuco_ids)
+			
+			# Draw the Charuco board we've detected to show our calibrator the board was properly detected
+			img = aruco.drawDetectedCornersCharuco(
+					image=img,
+					charucoCorners=charuco_corners,
+					charucoIds=charuco_ids)
+		
+			# If our image size is unknown, set it now
+			if not image_size:
+				image_size = gray.shape[::-1]
+			
+			# Reproportion the image, maxing width or height at 1000
+			proportion = max(img.shape) / 1000.0
+			img = cv2.resize(img, (int(img.shape[1]/proportion), int(img.shape[0]/proportion)))
+			# Pause to display each image, waiting for key press
+			cv2.imshow('Charuco board', img)
+			if cv2.waitKey(0) == ord('q'):
+				break
+
+			validCaptures += 1
+			if validCaptures == 40:
+				print("40 captures")
+				break
+
+	# Destroy any open CV windows
+	cv2.destroyAllWindows()
+
+	# Now that we've seen all of our images, perform the camera calibration
+	# based on the set of points we've discovered
+	calibration, cameraMatrix, distCoeffs, rvecs, tvecs = aruco.calibrateCameraCharuco(
+			charucoCorners=corners_all,
+			charucoIds=ids_all,
+			board=CHARUCO_BOARD,
+			imageSize=image_size,
+			cameraMatrix=None,
+			distCoeffs=None)
+			
+	# Print matrix and distortion coefficient to the console
+	print(cameraMatrix)
+	print(distCoeffs)
+			
+	# Save values to be used where matrix+dist is required, for instance for posture estimation
+	# I save files in a pickle file, but you can use yaml or whatever works for you
+	f = open('./calibration/MarkerCalibration.pckl', 'wb')
+	pickle.dump((cameraMatrix, distCoeffs, rvecs, tvecs), f)
+	f.close()
+			
+	# Print to console our success
+	print('Calibration successful. Calibration file used: {}'.format('MarkerCalibration.pckl'))
+else:
+	print('Camera calibration found, continueing to projector calibration.')
+	f = open('./calibration/MarkerCalibration.pckl', 'rb')
+	(cameraMatrix, distCoeffs, rvecs, tvecs) = pickle.load(f)
+	f.close()
+	if cameraMatrix is None or distCoeffs is None:
+		print("Calibration issue. Remove ./MarkerCalibration.pckl and recalibrate your camera with CalibrateCamera.py.")
+		exit()
+
+# Ray-Plane Intersection Algorithm
+def intersectCirclesRaysToBoard(circles, rvec, t, cameraMatrix, dist_coef):
+	circles_normalized = cv2.convertPointsToHomogeneous(cv2.undistortPoints(circles, cameraMatrix, dist_coef))
+ 
+	if len(rvec) == 0:
+		return None
+
+	rvec = np.array(rvec)
+	t = np.array(t)
+
+	R, _ = cv2.Rodrigues(rvec)
+ 
+	# https://stackoverflow.com/questions/5666222/3d-line-plane-intersection
+ 
+	plane_normal = R[2,:] # last row of plane rotation matrix is normal to plane
+	plane_point = t.T	 # t is a point on the plane
+ 
+	epsilon = 1e-06
+ 
+	circles_3d = np.zeros((0,3), dtype=np.float32)
+ 
+	for p in circles_normalized:
+		ray_direction = p / np.linalg.norm(p)
+		ray_point = p
+ 
+		ndotu = plane_normal.dot(ray_direction.T)
+ 
+		if abs(ndotu) < epsilon:
+			print ("no intersection or line is within plane")
+ 
+		w = ray_point - plane_point
+		si = -plane_normal.dot(w.T) / ndotu
+		Psi = w + si * ray_direction + plane_point
+
+		circles_3d = np.append(circles_3d, Psi, axis = 0)
+	
+	return circles_3d
+
+# --------- detect circles -----------
+# 3D Points
+circles3D, circles3D_reprojected = None, None
+circles_grid_size = (6, 7)
+
+cap = cv2.VideoCapture('ChAruco_Circles.webm')
+validCaptures = 0
+
+# Loop through images glob'ed
+while cap.isOpened():
+	ret, img = cap.read()
+	if ret is False:
+		break
+	
+	cv2.imshow('CircleBoard', img)
+	if cv2.waitKey(1) == ord('q'):
+		break
+
+	# Grayscale the image
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	""" Detect Circle Grid """
+	params = cv2.SimpleBlobDetector_Params()
+	params.filterByColor=1 
+	params.blobColor=255
+	params.filterByArea = True
+	params.minArea=25
+	params.maxArea=800
+	# params.minDistBetweenBlobs=5
+
+	# Set up the detector with default parameters.
+	detector = cv2.SimpleBlobDetector_create(params)
+
+	ret, circles = cv2.findCirclesGrid(gray, circles_grid_size, flags=cv2.CALIB_CB_SYMMETRIC_GRID, blobDetector = detector)
+	if ret is False or circles is None:
+		continue
+
+	# Draw Circles Centers
+	img = cv2.drawChessboardCorners(img, circles_grid_size, circles, ret)
+
+	""" Detect Aruco markers """
+	corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+	# Refine detected markers
+	# Eliminates markers not part of our board, adds missing markers to the board
+	corners, ids, rejectedImgPoints, recoveredIds = aruco.refineDetectedMarkers(
+			image = gray,
+			board = CHARUCO_BOARD,
+			detectedCorners = corners,
+			detectedIds = ids,
+			rejectedCorners = rejectedImgPoints,
+			cameraMatrix = cameraMatrix,
+			distCoeffs = distCoeffs)   
+	img = aruco.drawDetectedMarkers(img, corners, borderColor=(0, 0, 255))
+	
+	# Only try to find CharucoBoard if we found markers
+	if ids is not None and len(ids) >= 24:
+		# Get charuco corners and ids from detected aruco markers
+		response, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(
+				markerCorners=corners,
+				markerIds=ids,
+				image=gray,
+				board=CHARUCO_BOARD)
+		# Estimate the posture of the charuco board, which is a construction of 3D space based on the 2D video 
+		pose, rvec, tvec = aruco.estimatePoseCharucoBoard(
+				charucoCorners=charuco_corners, 
+				charucoIds=charuco_ids, 
+				board=CHARUCO_BOARD, 
+				cameraMatrix=cameraMatrix, 
+				distCoeffs=distCoeffs)
+		if pose:
+			""" 
+			Only use frames that reach this point! 
+			If they reach here, that means we have a valid Circle grid and ChAruco pose
+			"""
+			imgpoints.append(circles)
+			# Draw the camera posture calculated from the gridboard
+			img = aruco.drawAxis(img, cameraMatrix, distCoeffs, rvec, tvec, 0.1)
+
+			""" Perform Ray-Plane Intersection to find 3D points """
+			# ray-plane intersection: circle-center to chessboard-plane
+			circles3D = intersectCirclesRaysToBoard(circles, rvec, tvec, cameraMatrix, distCoeffs)
+			# objpoints.append(circles3D.astype('float32'))
+			objpoints.append(objp)
+
+			# re-project on camera for verification
+			circles3D_reprojected, _ = cv2.projectPoints(circles3D, (0,0,0), (0,0,0), cameraMatrix, distCoeffs)
+			imgpointsProj.append(circles3D_reprojected.astype('float32'))
+
+			for c in circles3D_reprojected:
+				cv2.circle(img, tuple(c.astype(np.int32)[0]), 3, (255,255,0), cv2.FILLED)
+
+			validCaptures += 1
+			if validCaptures == 120:
+				break
+
+			cv2.imshow('CircleBoard', img)
+			if cv2.waitKey(0) == ord('q'):
+				break
+
+# Destroy any open CV windows
+cap.release()
+cv2.destroyAllWindows()
+
+# calibrate projector
+print("calibrate projector")
+K_proj = np.zeros(shape=(3,3))
+K_proj[0][0] = 1
+K_proj[1][1] = 1
+K_proj[0][2] = 1
+K_proj[1][2] = 1
+K_proj[2][2] = 1
+
+ret, K_proj, dist_coef_proj, rvecs, tvecs = cv2.calibrateCamera(
+	objpoints,
+	imgpointsProj,
+	gray.shape[::-1],
+	None,
+	None,
+	flags=cv2.CALIB_FIX_INTRINSIC
+)
+print("proj calib mat after\n%s"%K_proj)
+print("proj dist_coef %s"%dist_coef_proj.T)
+print("calibration reproj err %s"%ret)
+
+print("stereo calibration")
+ret, K, dist_coef, K_proj, dist_coef_proj, proj_R, proj_T, _, _ = cv2.stereoCalibrate(
+	objpoints,
+	imgpoints,
+	imgpointsProj,
+	cameraMatrix,
+	distCoeffs,
+	K_proj,
+	dist_coef_proj,
+	gray.shape[::-1],
+	flags=cv2.CALIB_FIX_INTRINSIC
+)
+
+proj_rvec, _ = cv2.Rodrigues(proj_R)
+
+print("R \n%s"%proj_R)
+print("T %s"%proj_T.T)
+print("proj calib mat after\n%s"%K_proj)
+print("proj dist_coef %s"	   %dist_coef_proj.T)
+print("cam calib mat after\n%s" %K)
+print("cam dist_coef %s"		%dist_coef.T)
+print("reproj err %f"%ret)
+
+f = open('./calibration/ProCamCalibration.pckl', 'wb')
+pickle.dump((proj_R, proj_T.T, K_proj, dist_coef_proj.T, K, dist_coef.T, ret), f)
+f.close()
---
+++ pose_markerPerpTrans.py
@@ -0,0 +1,218 @@
+# The following code is used to watch a video stream, detect Aruco markers, and use
+# a set of markers to determine the posture of the camera in relation to the plane
+# of markers.
+#
+# Assumes that all markers are on the same plane, for example on the same piece of paper
+#
+# Requires camera calibration (see the rest of the project for example calibration)
+
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import os
+import pickle
+import sys
+
+# Check for camera calibration data
+if not os.path.exists('./calibration/ProCamCalibration3.pckl'):
+    print("You need to calibrate the camera you'll be using. See calibration project directory for details.")
+    exit()
+else:
+    f = open('./calibration/ProCamCalibration3.pckl', 'rb')
+    (_, _, _, _, cameraMatrix, distCoeffs, _) = pickle.load(f)
+    f.close()
+    if cameraMatrix is None or distCoeffs is None:
+        print("Calibration issue. Remove ./calibration/ProCamCalibration3.pckl and recalibrate your camera with CalibrateCamera.py.")
+        exit()
+
+def onClick(event, x, y, flags, params):
+    global corner
+    # left-click event value is 2
+    if event == 1:
+        # store the coordinates of the click event
+        cornersTransform[corner] = [x,y]
+        corner += 1
+
+def four_point_transform(image, cornersTransform):
+
+	(tl, tr, br, bl) = cornersTransform
+ 
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+ 
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+ 
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+ 
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(cornersTransform, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+ 
+	# return the warped image
+	return warped
+
+def drawCube(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1,2)
+
+    # draw ground floor in green
+    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+
+    # draw pillars in blue color
+    for i,j in zip(range(4),range(4,8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+
+    # draw top layer in red color
+    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+
+    return img
+
+# Constant parameters used in Aruco methods
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+
+# Create grid board object we're using in our stream
+board = aruco.GridBoard_create(
+        markersX=1,
+        markersY=1,
+        markerLength=0.09,
+        markerSeparation=0.01,
+        dictionary=ARUCO_DICT)
+
+cornersTransform = np.zeros((4, 2), dtype="float32")
+corner = 0
+
+# Create vectors we'll be using for rotations and translations for postures
+rvecs, tvecs = None, None
+axis = np.float32([[-.5,-.5,0], [-.5,.5,0], [.5,.5,0], [.5,-.5,0],
+                   [-.5,-.5,1],[-.5,.5,1],[.5,.5,1],[.5,-.5,1] ])
+
+aruco1 = cv2.imread('./patterns/test_marker1.jpg')
+aruco2 = cv2.imread('./patterns/test_marker2.jpg')
+aruco3 = cv2.imread('./patterns/test_marker3.jpg')
+aruco4 = cv2.imread('./patterns/test_marker4.jpg')
+
+transformed = False
+height = 720
+width = 1280
+
+# Make output image fullscreen
+cv2.namedWindow('ProjectImage',cv2.WINDOW_NORMAL)
+cv2.namedWindow('InputImage',cv2.WINDOW_NORMAL)
+# cv2.setWindowProperty('ProjectImage', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cam.set(3, 1280)
+cam.set(4, 720)
+while(cam.isOpened()):
+    # Capturing each frame of our video stream
+    ret, ProjectImage = cam.read()
+    if ret == True:
+        # grayscale image
+        gray = cv2.cvtColor(ProjectImage, cv2.COLOR_BGR2GRAY)
+
+        # Display our image
+        cv2.imshow('InputImage', ProjectImage)
+        cv2.setMouseCallback('InputImage', onClick)
+
+        # Make background black
+        ProjectImage = np.zeros((720, 1280, 3), np.uint8)
+        ProjectImage[:] = (255, 255, 255)
+        ProjectImage[ 0:0+aruco1.shape[0], 0:0+aruco1.shape[1]] = aruco1
+        ProjectImage[ 0:0+aruco1.shape[0], 0-aruco1.shape[1]:width] = aruco2
+        ProjectImage[ 0-aruco1.shape[0]:height, 0:0+aruco1.shape[1]] = aruco3
+        ProjectImage[ 0-aruco1.shape[0]:height, 0-aruco1.shape[1]:width] = aruco4
+        
+        # Detect Aruco markers
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+  
+        # Refine detected markers
+        # Eliminates markers not part of our board, adds missing markers to the board
+        corners, ids, rejectedImgPoints, recoveredIds = aruco.refineDetectedMarkers(
+                image = gray,
+                board = board,
+                detectedCorners = corners,
+                detectedIds = ids,
+                rejectedCorners = rejectedImgPoints,
+                cameraMatrix = cameraMatrix,
+                distCoeffs = distCoeffs)   
+
+        # Outline all of the markers detected in our image
+        # ProjectImage = aruco.drawDetectedMarkers(ProjectImage, corners, ids, borderColor=(0, 0, 255))
+        ProjectImage = aruco.drawDetectedMarkers(ProjectImage, corners, borderColor=(0, 0, 255))
+        # Draw the Charuco board we've detected to show our calibrator the board was properly detected
+    
+        # Aruco marker found
+        imgptsAll = []
+        if ids is not None and len(ids) > 0:
+            # Estimate the posture per each Aruco marker
+            rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, 1, cameraMatrix, distCoeffs)
+
+            for rvec, tvec in zip(rvecs, tvecs):
+                # project 3D points to image plane
+                try:
+                    imgpts, jac = cv2.projectPoints(axis, rvec, tvec, cameraMatrix, distCoeffs)
+                    imgptsAll.append(imgpts)
+                except:
+                    continue
+                
+                if len(sys.argv) == 2 and sys.argv[1] == 'cube':
+                    ProjectImage = drawCube(ProjectImage, corners, imgpts)    
+                # ProjectImage = aruco.drawAxis(ProjectImage, cameraMatrix, distCoeffs, rvec, tvec, 1)
+        
+            # If four corners found, transform image
+            topLeft = [1]
+            topRight = [2]
+            botLeft = [3]
+            botRight = [4]
+            if (topLeft in ids) and (topRight in ids) and (botLeft in ids) and (botRight in ids) and not transformed:
+                print(imgptsAll[np.where(ids == topLeft)[0][0]])
+                print(imgptsAll[np.where(ids == topLeft)[0][0]][0][0])
+                cornersTransform[0] = imgptsAll[np.where(ids == topLeft)[0][0]][1][0]
+                cornersTransform[1] = imgptsAll[np.where(ids == topRight)[0][0]][2][0]
+                cornersTransform[2] = imgptsAll[np.where(ids == botRight)[0][0]][3][0]
+                cornersTransform[3] = imgptsAll[np.where(ids == botLeft)[0][0]][0][0]
+                # print(cornersTransform)
+                ProjectImage = four_point_transform(ProjectImage, cornersTransform)
+                transformed = True
+            
+            if transformed:
+                ProjectImage = four_point_transform(ProjectImage, cornersTransform)
+
+        ProjectImage[ 0:0+aruco1.shape[0], 0:0+aruco1.shape[1]] = aruco1
+        ProjectImage[ 0:0+aruco1.shape[0], 0-aruco1.shape[1]:width] = aruco2
+        ProjectImage[ 0-aruco1.shape[0]:height, 0:0+aruco1.shape[1]] = aruco3
+        ProjectImage[ 0-aruco1.shape[0]:height, 0-aruco1.shape[1]:width] = aruco4
+
+        cv2.imshow('ProjectImage', ProjectImage)
+        cv2.imshow('ProjectImage2', ProjectImage)
+
+    # Reset corner calibration
+    if cv2.waitKey(1) == ord('r'):
+        print("Corners reset")
+        cornersTransform = np.zeros((4, 2), dtype="float32")
+        corner = 0
+    
+    # Exit at the end of the video on the 'q' keypress
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
